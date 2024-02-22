00755c12899c67d78bba41b181c86d7169d6743b
Geo Pertea <geo.pertea@gmail.com>
2024-02-22 16:21:45 -0500

    numbered scripts step-wise

---
+++ 01_calc_snpPCs.R
@@ -0,0 +1,85 @@
+#!/bin/env Rscript
+## script to get MDS and snpPCs for a set of genotypes in a VCF.gz input data
+## on JHPCE, the plink module must be loaded (plink must be available in PATH)!
+library(data.table)
+## change here the path to the genotype file to calculate MDS for:
+#gtinput='genotypes/gt_bsp12_n114.vcf.gz' ## if you have a vcf
+gtinput='genotypes/gt_bsp12_n114_rIDs.bed' ## if you have a plink bed file instead of vcf
+
+## if you have a plink file instead of vcf, that you want to QC and use for MDS calculation
+## set gtinput to the .bed file directly (NOTE: include the .bed extension here!) but
+## set bedout to a different prefix! (to prevent overwriting)
+
+bedout="genotypes/gt_bsp12_rIDs_MDS"
+
+## write the bed files with this bedout prefix
+## change the output prefix as needed
+#bedout=sub('.vcf.gz', '', vcf, fixed=T)
+
+## QC filter
+qcflt='--geno 0.01 --maf 0.001 --hwe 0.0001'
+
+## this should probably not be applied to an already extracted VCF that has all the samples of interest
+## applying the maf QC filter for very small sets may remove too many variants
+## e.g. for a file with 68 genotypes, using qcflt like this can eliminate up to a half of the variants!
+plink2=Sys.which('plink2')
+if (nchar(plink2)==0) stop("plink2 not found in PATH!")
+
+plink=Sys.which('plink')
+if (nchar(plink)==0) stop("plink not found in PATH!")
+
+## gtinput file must exists
+if (!file.exists(gtinput)) stop(paste("Cannot find input genotype file:", gtinput))
+
+
+is.vcf=TRUE
+in.fmt=paste('--vcf', gtinput)
+if (grepl('\\.bed$', gtinput)) {
+  is.vcf=FALSE
+  gtinput=sub('\\.bed$', '', gtinput)
+  in.fmt=paste('--bfile', gtinput)
+}
+
+## convert vcf to plink BED (or just QC filter the input BED if given directly)
+## NOTE: remove qcflt if this is a very small sub-sample already
+cmd=paste(plink2,"--threads 8 --make-bed --not-chr X --output-chr chrM",
+          in.fmt, qcflt, '--out',   bedout)
+message(" Running command:\n",cmd)
+  ## add '--keep samples_to_extract.txt' if only a subset of genotyping samples are needed
+r=system(cmd)
+if (r!=0) stop("Error in plink2 command!")
+
+indfile=paste0(bedout, '_indep')
+
+### independent and cluster
+## --indep <window size>['kb'] <step size (variant ct)> <VIF threshold>
+## produce a pruned subset of markers that are in approximate linkage equilibrium with each other
+## --indep requires three parameters:
+##          a window size in variant count or kilobase (if the 'kb' modifier is present) units,
+##          a variant count to shift the window at the end of each step,
+##          a variance inflation factor (VIF) threshold
+cmd = paste(plink, "--bfile", bedout, "--indep 100 10 1.25 --out", bedout)
+system(cmd)
+
+## MDS components
+## use "--mds-plot 15" below if you want 15 SNPs
+cmd = paste0(plink, " --bfile ", bedout,
+             " --cluster --mds-plot 10 --extract ", bedout, ".prune.in --out ", bedout)
+system(cmd)
+
+# ## A transpose
+cmd = paste(plink, "--bfile", bedout, "--recode A-transpose --out", bedout)
+system(cmd)
+
+#### read in MDS
+mds = read.table(paste0(bedout, ".mds"), header=TRUE,as.is=TRUE)
+rmds = mds[,-(1:3)] #remove FID, IID and SOL columns 1-3
+colnames(rmds) = paste0("snpPC",1:ncol(rmds))
+
+## WARNING: the assumption here is that FID does not matter (it is not used)
+rmds$SAMPLE_ID=mds$IID
+
+setcolorder(rmds, 'SAMPLE_ID')
+
+## write snpPCs file as csv
+fwrite(rmds, file=paste0(bedout, ".snpPCs.tab"), sep='\t')
--- 02_prep_tensorQTL_RSE_input.R
+++ 02_prep_tensorQTL_RSE_input.R
@@ -1,8 +1,7 @@
+#!/bin/env Rscript
 library(SummarizedExperiment)
 library(sessioninfo)
 library(tidyverse)
-#library(VariantAnnotation)
-#library(jaffelab)
 library(recount)
 library(data.table)
 library(qs)
@@ -13,23 +12,38 @@ library(sva)
 fgrse <- './rdata/rse_gene_n114.rda'
 
 ## path to SNP PCs file prepared with calc_snpPCs.R
-fsnp_pcs <- 'genotypes/gt_bsp12_MDS.snpPCs.csv'
+#fsnp_pcs <- 'genotypes/gt_bsp12_MDS_snpPCs.csv'
+fsnp_pcs <- 'genotypes/gt_bsp12_rIDs_MDS.snpPCs.tab'
+## note: tensorQTL script should use the matching genotypes/gt_bsp12_rIDs_MDS.bed as input!
 
-## model with known covariates to use in the eQTL analysis
+#### model with known covariates to use in the eQTL analysis ###
 modelstr='~Sex + Age  + snpPC1 + snpPC2 + snpPC3 + snpPC4 + snpPC5'
+## note that SVA will be used to calculate feature PCs (SVs)
+## that are not accounted for in this base model
 
-fgeno2rna <- 'genoID_to_sampleID.csv' ## 2 column file with genotype IDs and RNA sample IDs
+dsname <- 'bsp12rIDs' ## dataset name prefix to use for all output files
+## NOTE: change this if you change the model or any input data!
+
+## file with genotype ID to RNAseq SAMPLE ID mapping
+## can be set to NULL if the genoID is the same as the RNA SAMPLE_ID
+fgeno2rna <- NULL
+#fgeno2rna <- 'genoID_to_sampleID.csv' ## 2 column file with genotype IDs and RNA sample IDs
 ## must be without header, 1st column: genotype IDs, 2nd column: RNA SAMPLE_IDs as in the RSEs
+## the same file must be given to the python script that runs tensorQTL
 ## NOTE: colnames of the RSEs must match the RNA SAMPLE_IDs !
-## ### --------------------------------------------
+#----------------------
+
+geno2rna <- NULL
 
 if (!file.exists(fgrse)) stop(paste("Cannot find gene RSE file", fgrse))
 if (!file.exists(fsnp_pcs)) stop(paste("Cannot find gene SNP PCs file", f_snp_pcs))
-if (!file.exists(fgeno2rna)) stop(paste("Cannot find genotype ID to RNAseq SAMMPLE ID file", fgeno2rna))
-geno2rna <- fread(fgeno2rna, header=F, data.table=F)
-colnames(geno2rna) <- c('genoID', 'SAMPLE_ID')
 
-rses <- list()
+if (!is.null(fgeno2rna) && nchar(fgeno2rna)>1) {
+  if (!file.exists(fgeno2rna)) stop(paste("Cannot find genotype ID to RNAseq SAMMPLE ID file", fgeno2rna))
+  geno2rna <- fread(fgeno2rna, header=F, data.table=F)
+  colnames(geno2rna) <- c('genoID', 'SAMPLE_ID')
+}
+
 fn <- basename(file.path(fgrse))
 fdir <- dirname(file.path(fgrse))
 fpat <- paste0(sub('_gene', '_.+', fn, ignore.case = T), '$')
@@ -82,52 +96,82 @@ covar_format <- function(data) {
 }
 
 
-pd <- NULL ## phenodata used for all RSEs
 odir='eqtl_inputs' ## output directory for expression and covariate files
 if (!dir.exists(odir)) dir.create(odir)
 
 snpPCs <- fread(fsnp_pcs, data.table=F)
-genoIDs <- setdiff(geno2rna$genoID, snpPCs$SAMPLE_ID)
-rownames(snpPCs) <- snpPCs$SAMPLE_ID ## this is the genotype ID
-if (length(genoIDs)>0) {
-  stop("Error: genotype IDs lacking snpPCs: ", paste(genoIDs, collapse=', '))
+if (!is.null(geno2rna)) {
+   genoIDs <- setdiff(geno2rna$genoID, snpPCs$SAMPLE_ID)
+   if (length(genoIDs)>0) {
+     stop("Error: genotype IDs lacking snpPCs: ", paste(genoIDs, collapse=', '))
+   }
 }
+rownames(snpPCs) <- snpPCs$SAMPLE_ID ## this is the genotype ID
 
 ## use a basic expression cutoff
 EXPR_CUTOFF=0.2
+pd <- NULL ## phenodata used for all RSEs
+model <- NULL
 #modmats <- list()
 for (i in 1:length(frses)) {
   rse <- get(load(file = frses[[i]], verbose=F)) # load the RSE
+  rm(list=ls(pattern='^rse_'))
   feature <- names(frses)[i]
   ## subset RSE by geno2rna$SAMPLE_ID, which should match colnames(pd)
   nc <- ncol(rse)
-  rse <- rse[ ,colnames(rse) %in% geno2rna$SAMPLE_ID]
+  if (is.null(geno2rna)) {
+    rse <- rse[ ,colnames(rse) %in% snpPCs$SAMPLE_ID]
+  } else {
+    rse <- rse[ ,colnames(rse) %in% geno2rna$SAMPLE_ID]
+  }
   if (ncol(rse)<nc/2) {
-    stop(paste("Error: too many RSE samples do not match the SAMPLE ID in mapping file ",
-               fgeno2rna))
+    stop("Too many RSE samples do not match the SAMPLE ID with genotype data!")
   }
   if (is.null(pd)) {
     pd <- as.data.frame(colData(rse))
-    ## reorder geno2rna to match order in colnames(pd)
-    ## append genoID to pd
-    rownames(geno2rna) <- geno2rna$SAMPLE_ID
-    geno2rna <- geno2rna[rownames(pd), ]
-    stopifnot(identical(rownames(pd), rownames(geno2rna)))
-    pd <- cbind(pd, data.frame(genoID = geno2rna$genoID))
-    snpPCs <- snpPCs[pd$genoID, ]
-    stopifnot(identical(pd$genoID, rownames(snpPCs)))
-    stopifnot(identical(pd$genoID, rownames(snpPCs)))
-  }
-  stopifnot(identical(colnames(rse), rownames(pd)))
-  if (feature!='tx' & !is.null(assays(rse)$rpkm)) {
-    if (grepl('^jx', feature) & is.null(rowData(rse)$Length)) {
-        rowData(rse)$Length <- 100 ##junctions
+    if (is.null(geno2rna)) {
+      ## SAMPLE_ID is the same as genotype ID
+      missIDs <- setdiff(rownames(pd), rownames(snpPCs))
+      if (length(missIDs)>0) {
+        stop("Error: RNA SAMPLE_IDs lacking snpPCs: ", paste(missIDs, collapse=', '))
+      }
+      snpPCs <- snpPCs[rownames(pd), ] #subset/reorder snpPCs
+      stopifnot(identical(rownames(pd), rownames(snpPCs)))
+      pd <- cbind(pd, snpPCs[, grep('^snpPC\\d+', colnames(snpPCs))])
+    } else {
+      ## reorder geno2rna to match order in colnames(pd)
+      ## append genoID to pd
+      rownames(geno2rna) <- geno2rna$SAMPLE_ID
+      geno2rna <- geno2rna[rownames(pd), ]
+      stopifnot(identical(rownames(pd), rownames(geno2rna)))
+      pd <- cbind(pd, data.frame(genoID = geno2rna$genoID))
+      snpPCs <- snpPCs[pd$genoID, ]
+      stopifnot(identical(pd$genoID, rownames(snpPCs)))
+      pd <- cbind(pd, snpPCs[, grep('^snpPC\\d+', colnames(snpPCs))])
     }
-    assays(rse)$rpkm <- recount::getRPKM(rse, 'Length')
+
+    ## model (sample) factors and covariates
+    model <- model.matrix(as.formula(modelstr), data = pd) [, -1]
+    stopifnot(identical(rownames(model), rownames(pd)))
+  } # pd and model buing build for the first RSE
+  stopifnot(identical(colnames(rse), rownames(pd)))
+  if (feature=='jx' & is.null(assays(rse)$rpkm) & !is.null(assays(rse)$rp10m)) {
+    names(assays(rse)) <- sub('rp10m', 'rpkm', names(assays(rse)))
+  }
+  if (feature=='tx') {
+    stopifnot(!is.null(assays(rse)$tpm))
+  } else { # not tx, must have rpkm
+    if (is.null(assays(rse)$rpkm)) {
+      stopifnot(!is.null(assays(rse)$counts))
+      if (grepl('^jx', feature) & is.null(rowData(rse)$Length)) {
+          rowData(rse)$Length <- 100 ##junctions
+      }
+      assays(rse)$rpkm <- recount::getRPKM(rse, 'Length')
+    }
   }
   ## apply expression cutoff
   if (feature=='tx') {
-    rse <- rses[rowMeans(assays(rse)$tpm)>EXPR_CUTOFF, ]
+    rse <- rse[rowMeans(assays(rse)$tpm)>EXPR_CUTOFF, ]
   } else {
     rse <- rse[rowMeans(assays(rse)$rpkm)>EXPR_CUTOFF, ]
   }
@@ -135,7 +179,8 @@ for (i in 1:length(frses)) {
   rse <- rse[!grepl('chr[YM]', seqnames(rowRanges(rse))), ]
 
   ## calculate feature PCs (sva)
-  fn=paste0(odir, '/', feature,'.featurePCs.qs')
+  fn=paste0(odir, '/', dsname, '.', feature,'.featurePCs.qs')
+  ffPCs <- NULL
   if (file.exists(fn)) {
     cat("Loading", feature, "feature SVs from", fn, "\n")
     ffPCs <- qread(fn)
@@ -149,33 +194,31 @@ for (i in 1:length(frses)) {
     pca <- prcomp(t(lmx))
     k <- 0
     if (dim(lmx)[1]>60000) {
-      k <- num.sv(lmx, mod, vfilter = 60000)
+      k <- num.sv(lmx, model, vfilter = 60000)
     } else {
-      k <- num.sv(lmx, mod)
+      k <- num.sv(lmx, model)
     }
     ffPCs <- pca$x[, 1:k]
     qsave(ffPCs, file=fn)
     cat("     saved as",fn, "\n")
   }
-  pc <- ffPCs
-  stopifnot(identical(rownames(pc), colnames(rse)))
+
+  stopifnot(identical(rownames(ffPCs), colnames(rse)))
+
   cat(" exporting data for feature",feature," ..\n")
-  model <- model.matrix(as.formula(modelstr), data = pd) [, -1]
-  ## model (sample) factors and covariates
-  stopifnot(identical(rownames(model), rownames(pd)))
 
   cpd <- covar_format(model)
   ## PC data, SVs (feature) covariates
-  stopifnot(identical(rownames(pc), rownames(pd)))
-  cpc <- covar_format(pc)
+  stopifnot(identical(rownames(ffPCs), rownames(pd)))
+  cpc <- covar_format(ffPCs)
   ## bind model (sample) covars with feature covars (SVs) and save
   stopifnot(identical(colnames(cpd), colnames(cpc)))
   covars <- rbind(cpd, cpc)
-  fwrite(covars, file = paste0(odir, '/', reg,'.', feature, '.covars.txt'),
+  fwrite(covars, file = paste0(odir, '/', dsname, '.', feature, '.covars.txt'),
          sep = "\t", quote = FALSE, row.names = FALSE)
 
   bed <- rse2bed(rse, feature)
-  fbed=paste0(odir, '/',feature,'.expr.bed.gz')
+  fbed=paste0(odir, '/', dsname, '.', feature,'.expr.bed.gz')
   fwrite(bed, file = fbed, sep = "\t", quote = FALSE, row.names = FALSE)
   rm(bed)
   cat("  ..exported to", fbed, "\n")
